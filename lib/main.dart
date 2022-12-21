c4d257cf9d89b50fec7626133eae378cd66e37a1
Morné le Roux <mornelerx@gmail.com>
2022-12-21 11:48:16 +0200

    removed dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import "package:firebase_core/firebase_core.dart";
-import 'package:picstate/screens/login_screen.dart';
-import "firebase_options.dart";
+import 'package:picstate/screens/home_screen.dart';
 
-void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const PicState());
 }
 
@@ -18,7 +15,7 @@ class PicState extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: HomeScreen(),
     );
   }
 }
