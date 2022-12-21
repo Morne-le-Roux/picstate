c5b6a48071414878dad00338fb5b82dae0bb24cb
Morné le Roux <mornelerx@gmail.com>
2022-12-21 16:31:36 +0200

    Implemented supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:picstate/screens/home_screen.dart';
+import 'supabase_settings.dart';
 
-void main() {
+//!UNCOMMENT THE BELOW AND INSERT YOUR SUPABASE DETAILS
+
+// final supabaseUrl = 'yourSupaBaseUrl';
+// final supabaseKey = const String.fromEnvironment('SUPABASE_KEY');
+
+//! or create a supabase_settings.dart in your lib folder, add your details there with the above variables, and import the file.
+
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const PicState());
 }
 
