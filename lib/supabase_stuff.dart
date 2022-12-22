ca5e92917d28db0be6e958b192b5dbb8a546e036
Morné le Roux <mornelerx@gmail.com>
2022-12-22 12:15:19 +0200

    added supabase functions

--- lib/supabase_stuff.dart
+++ lib/supabase_stuff.dart
@@ -1,4 +1,26 @@
+import 'package:picstate/supabase_settings.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+class SupaBaseDoStuff {
+  final client = SupabaseClient(supabaseUrl, supabaseKey);
 
+  addData(String taskValue, bool statusValue) {
+    client.from("tasktable").insert({"task": taskValue, "status": statusValue});
+  }
 
-class SupaBaseDoStuff
+  readData() async {
+    final dataList = await client
+        .from("tasktable")
+        .select()
+        .order("task", ascending: true) as List;
+    return dataList;
+  }
+
+  updateData(int id, bool statusValue) async {
+    client.from("tasktable").update({"status": statusValue}).eq("id", id);
+  }
+
+  deleteData(int id) {
+    client.from("tasktable").delete().eq("id", id);
+  }
+}
