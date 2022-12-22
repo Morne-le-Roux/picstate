3adc6fbdbb852d07c6b97d3d3ca2ddd9c1dbb1b4
Morné le Roux <mornelerx@gmail.com>
2022-12-22 11:52:45 +0200

    bugstuff

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'package:picstate/screens/login_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'supabase_settings.dart';
 
-//!UNCOMMENT THE BELOW AND INSERT YOUR SUPABASE DETAILS
-
-// final supabaseUrl = 'yourSupaBaseUrl';
-// final supabaseKey = const String.fromEnvironment('SUPABASE_KEY');
-
-//! or create a supabase_settings.dart in your lib folder, add your details there with the above variables, and import the file.
+//! create a supabase_settings.dart in your lib folder, add your details there with the below variables, and import the file.
+// String supabaseUrl = 'yourSupaBaseUrl';
+// String supabaseKey = 'yourSupabaseKey';
 
 Future<void> main() async {
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
