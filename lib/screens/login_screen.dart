990d6968e64ceb3f440a96c0581bea228e47fce8
Morné le Roux <mornelerx@gmail.com>
2023-01-05 20:43:46 +0200

    changed default func

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,11 +19,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final SupaBaseStuff _supaBaseStuff = SupaBaseStuff();
-  // String _password = "";
-  // String _email = "";
-
-  String _password = "123456";
-  String _email = "mornelerx@gmail.com";
+  String _password = "";
+  String _email = "";
 
   ///
   ///
