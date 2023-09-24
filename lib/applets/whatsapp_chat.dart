1bb33841cac45c04d54c5c94cd5fa4fbfc21bff7
Morné le Roux <mornelerx@gmail.com>
2023-09-24 19:06:48 +0200

    fixed whatsapp not launching

--- lib/applets/whatsapp_chat.dart
+++ lib/applets/whatsapp_chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:picstate/custom_widgets/rounded_button.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class WhatsappChatDialer extends StatefulWidget {
@@ -10,26 +11,25 @@ class WhatsappChatDialer extends StatefulWidget {
 
 class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
   //VARIABLES
-  String countryCode = "";
+
   String phoneNumber = "";
-  String api = "https://wa.me/";
+
+  // String api = "https://wa.me/";
   String completedLink = "";
 
 //FUNCTION TO LAUNCH WHATSAPP URL
   Future<void> launchWhatsapp({required url}) async {
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      throw "Could not launch $url";
-    }
+    await launchUrl(url);
   }
+
+  //whatsapp://send/?phone=27710908116&text&type=phone_number&app_absent=0 could not be loaded because:
 
   final TextEditingController _countryCode = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     _countryCode.text = 27.toString();
-    countryCode = _countryCode.text;
+
     return Container(
 //BACKGROUND COLOR
       color: Colors.white,
@@ -52,16 +52,18 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
                     border: Border.all(
+                        width: 3,
                         //BORDER COLOR
                         color: Colors.amber),
                     borderRadius: const BorderRadius.all(Radius.circular(10))),
                 child: TextField(
+                  keyboardType: TextInputType.phone,
                   controller: _countryCode,
                   decoration: const InputDecoration(
                       icon: Icon(Icons.map_outlined),
                       prefix: Text("+"),
                       border: InputBorder.none),
-                  onChanged: (value) => countryCode = _countryCode.text,
+                  onChanged: (value) => _countryCode.text = value,
                 ),
               ),
             ),
@@ -82,10 +84,12 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
                     border: Border.all(
+                        width: 3,
                         //BORDER COLOR
                         color: Colors.amber),
                     borderRadius: const BorderRadius.all(Radius.circular(10))),
                 child: TextField(
+                  keyboardType: TextInputType.phone,
                   decoration: const InputDecoration(
                       icon: Icon(Icons.phone_outlined),
                       border: InputBorder.none),
@@ -100,63 +104,42 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
           ),
 //BUTTON TO LAUNCH URL
 
-          GestureDetector(
-            child: Material(
-              borderRadius: const BorderRadius.all(Radius.circular(50)),
-              elevation: 3,
-              child: Container(
-                height: 50,
-                width: 200,
-                decoration: const BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(50),
-                  ),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Open Chat",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-
-//ON TAP
-
-            onTap: () {
-              completedLink = "$api$countryCode$phoneNumber";
+          RoundedButton(
+              text: "Open Chat",
+              onTap: () {
+                completedLink =
+                    "whatsapp://send/?phone=${_countryCode.text}$phoneNumber&text&type=phone_number";
 
 //CONDITIONALS TO CHECK
 
 //IF NO PHONE NUMBER IS INSERTED
-              if (phoneNumber == "") {
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("You cannot leave the phone number empty.")));
-
-//IF NO COUNTRY CODE IS INSERTED
-              } else {
-                if (countryCode == "") {
+                if (phoneNumber == "") {
                   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                       content:
-                          Text("You cannot leave the country code empty.")));
+                          Text("You cannot leave the phone number empty.")));
+
+//IF NO COUNTRY CODE IS INSERTED
+                } else {
+                  if (_countryCode.text == "") {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content:
+                            Text("You cannot leave the country code empty.")));
 
 //IF THERE IS A ZERO IN FRONT OF THE NUMBER
-                } else {
-                  if (phoneNumber.substring(0, 1) == "0") {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text(
-                            "Please remove the '0' in front of your phone number!")));
+                  } else {
+                    if (phoneNumber.substring(0, 1) == "0") {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text(
+                              "Please remove the '0' in front of your phone number!")));
 
 //IF EVERYTHING ELSE RUNS FINE
-                  } else {
-                    var uri = Uri.parse(completedLink);
-                    launchWhatsapp(url: uri);
+                    } else {
+                      var uri = Uri.parse(completedLink);
+                      launchWhatsapp(url: uri);
+                    }
                   }
                 }
-              }
-            },
-          ),
+              })
         ],
       ),
     );
