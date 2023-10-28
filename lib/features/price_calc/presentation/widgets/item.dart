e5511bcf34a6c73d180a854ed3cbf7824bc1fd40
Morné le Roux <mornelerx@gmail.com>
2023-10-28 16:26:10 +0200

    opens dialog for price calc

--- lib/features/price_calc/presentation/widgets/item.dart
+++ lib/features/price_calc/presentation/widgets/item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:picstate/config/constants.dart';
+import 'package:picstate/features/price_calc/presentation/widgets/invoice_book.dart';
 
 class Item extends StatelessWidget {
   final String name;
@@ -11,43 +12,53 @@ class Item extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Material(
-        elevation: 5,
-        borderRadius: BorderRadius.circular(20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 100),
-            //height of the widget
-            height: 60,
-            decoration: BoxDecoration(
-              //gradient settings
-              gradient: LinearGradient(
-                  //Based on Task inedx in the listviewbuilder, will invert the gradient so the tasks separate a bit better in list view
-                  begin: index % 2 == 0
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  end: index % 2 == 0
-                      ? Alignment.centerLeft
-                      : Alignment.centerRight,
-                  //gradient color
-                  colors: kColorTodo),
-            ),
-            child: Container(
-              margin: const EdgeInsets.all(4),
+    return InkWell(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return const InvoiceBook();
+          },
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Material(
+          elevation: 5,
+          borderRadius: BorderRadius.circular(20),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 100),
+              //height of the widget
+              height: 60,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
+                //gradient settings
+                gradient: LinearGradient(
+                    //Based on Task inedx in the listviewbuilder, will invert the gradient so the tasks separate a bit better in list view
+                    begin: index % 2 == 0
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    end: index % 2 == 0
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    //gradient color
+                    colors: kColorTodo),
               ),
-              child: Center(
-                child: Text(
-                  name,
-                  style: kTaskTextStyle.copyWith(fontSize: 20),
+              child: Container(
+                margin: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    name,
+                    style: kTaskTextStyle.copyWith(fontSize: 20),
+                  ),
                 ),
               ),
             ),
