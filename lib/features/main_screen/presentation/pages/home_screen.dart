9fdb297874b09e27338aa452b6c4eace9eaa23f6
Morné le Roux <mornelerx@gmail.com>
2023-11-20 21:13:16 +0200

    styling update

--- lib/features/main_screen/presentation/pages/home_screen.dart
+++ lib/features/main_screen/presentation/pages/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:picstate/features/main_screen/domain/usecases/needs_update.dart';
+import 'package:picstate/features/main_screen/presentation/widgets/side_view.dart';
 import 'package:picstate/features/price_calc/presentation/widgets/price_calculator_list.dart';
 import 'package:picstate/features/whatsapp_dialing/whatsapp_chat.dart';
 import 'package:picstate/config/constants.dart';
@@ -37,11 +38,11 @@ class _HomeScreenState extends State<HomeScreen> {
       content: "Jobs to be done",
       color: LinearGradient(colors: []),
     ),
-    const MenuButton(
-      heading: "Orders",
-      content: "Please Order",
-      color: LinearGradient(colors: []),
-    ),
+    // const MenuButton(
+    //   heading: "Orders",
+    //   content: "Please Order",
+    //   color: LinearGradient(colors: []),
+    // ),
     const MenuButton(
       heading: "Whatsapp Dialer",
       content: "Open Whatsapp Chat",
@@ -56,7 +57,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
 //currently selected button
   int selectedButton = -1;
-  int buttonRowCount = 2;
+  int buttonRowCount = 3;
+  bool sideViewVisible = false;
 
 //build
   @override
@@ -67,89 +69,102 @@ class _HomeScreenState extends State<HomeScreen> {
     if (selectedButton == 0) {
       widgetToshow = const ToDoStream();
     }
+
     if (selectedButton == 1) {
-      widgetToshow = const OrderStream();
-    }
-    if (selectedButton == 2) {
       widgetToshow = const WhatsappChatDialer();
     }
-    if (selectedButton == 3) {
+    if (selectedButton == 2) {
       widgetToshow = const PriceCalculatorList();
     }
 
     //gets screen width and ajusts columns
     double screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth >= 500) {
-      buttonRowCount = 4;
+    if (screenWidth >= 700) {
+      sideViewVisible = true;
     }
-    if (screenWidth <= 499) {
-      buttonRowCount = 2;
+    if (screenWidth <= 699) {
+      sideViewVisible = false;
     }
 
     //return
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'PRINT&IMAGE CENTRE',
-          style: kTaskTextStyle.copyWith(
-              fontStyle: FontStyle.italic, color: Colors.white60, fontSize: 30),
-        ),
-      ),
       backgroundColor: kBackgroundColor,
       body: SafeArea(
         top: false,
-        child: Column(
+        child: Flex(
+          direction: Axis.horizontal,
           children: [
-//Menu Buttons
-            GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: buttonRowCount,
-                childAspectRatio: 2,
-                children: List.generate(menuButtons.length, (index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedButton = index;
-                      });
-                    },
-                    child: MenuButton(
-                      heading: menuButtons[index].heading,
-                      content: menuButtons[index].content,
-                      color: selectedButton == index //is selected?
-                          ? kSelectedMenuButtonColor //color if selected
-                          : const LinearGradient(colors: [
-                              Colors.white,
-                              Colors.white
-                            ]), //color if not selected
-                    ),
-                  );
-                })),
-
-            const SizedBox(
-              height: 20,
-            ),
-//Shows selected item
-            widgetToshow,
-
-//Update Notifier (If not updated, display "Your app needs an update!")
-            Visibility(
-              replacement: const SizedBox.shrink(),
-              visible: needsUpdate,
+            Expanded(
+              flex: MediaQuery.of(context).size.width > 1200 ? 1 : 2,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Container(
-                    alignment: Alignment.center,
-                    height: 80,
-                    decoration: const BoxDecoration(color: Colors.red),
-                    child: Text(
-                      "Your app needs an update!",
-                      style: kTaskTextStyle.copyWith(color: Colors.white),
+                  //Menu Buttons
+                  GridView.count(
+                      shrinkWrap: true,
+                      crossAxisCount: buttonRowCount,
+                      childAspectRatio: 2,
+                      children: List.generate(menuButtons.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedButton = index;
+                            });
+                          },
+                          child: MenuButton(
+                            heading: menuButtons[index].heading,
+                            content: menuButtons[index].content,
+                            color: selectedButton == index //is selected?
+                                ? kSelectedMenuButtonColor //color if selected
+                                : const LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                        Colors.white,
+                                        Colors.white
+                                      ]), //color if not selected
+                          ),
+                        );
+                      })),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //Shows selected item
+                  widgetToshow,
+
+                  //Update Notifier (If not updated, display "Your app needs an update!")
+                  Visibility(
+                    replacement: const SizedBox.shrink(),
+                    visible: needsUpdate,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 80,
+                          decoration: const BoxDecoration(color: Colors.red),
+                          child: Text(
+                            "Your app needs an update!",
+                            style: kTaskTextStyle.copyWith(color: Colors.white),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
+            Visibility(
+                visible: sideViewVisible,
+                child: Expanded(
+                  flex: 2,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 0.1,
+                              color: const Color.fromARGB(255, 59, 59, 59))),
+                      child: const SideView()),
+                )),
           ],
         ),
       ),
