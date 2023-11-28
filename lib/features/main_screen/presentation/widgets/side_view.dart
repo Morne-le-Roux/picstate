10a3867b1a3a589c4d48f77e8530e950a78283b4
Morné le Roux <mornelerx@gmail.com>
2023-11-28 20:06:44 +0200

    updated sideview

--- lib/features/main_screen/presentation/widgets/side_view.dart
+++ lib/features/main_screen/presentation/widgets/side_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:picstate/config/constants.dart';
 import 'package:picstate/features/todo_list/presentation/widgets/task_widget.dart';
+import 'package:intl/intl.dart';
 
 class SideView extends StatelessWidget {
   const SideView({
@@ -9,6 +10,21 @@ class SideView extends StatelessWidget {
   });
 
   final TaskWidget? task;
+
+  parseCreatedAtDate(inputDate) {
+    String inputDateString = inputDate;
+
+// Parse the input string into a DateTime object
+    DateTime dateTime = DateTime.parse(inputDateString);
+
+// Create a custom date format
+    DateFormat dateFormat = DateFormat('dd-MM-yyyy / HH:mm');
+
+// Format the DateTime object as a string in the desired format
+    String formattedDateString = dateFormat.format(dateTime);
+
+    return formattedDateString; // Output: 22-09-2023 / 13:11
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +47,37 @@ class SideView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            //Heading
             Text(
               task!.taskName,
               overflow: TextOverflow.ellipsis,
-              style: kTaskTextStyle.copyWith(
-                  fontSize: 40, fontStyle: FontStyle.italic),
-            )
+              style: kTaskTextStyle.copyWith(fontSize: 40),
+            ),
+            //Created By
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Created By: ${task!.createdBy} \nAt ${parseCreatedAtDate(task!.createdAt)}",
+                  style: kHintTextStyle.copyWith(color: Colors.black),
+                ),
+                const SizedBox(width: 20),
+                //Due by
+                Text(
+                  "Due By: ${task!.dueDate}",
+                  style: kHintTextStyle.copyWith(color: Colors.black),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            Text("Description",
+                style: kTaskTextStyle.copyWith(
+                    fontSize: 20, fontStyle: FontStyle.italic)),
+            const SizedBox(height: 5),
+            Text(
+              task!.description,
+              style: kHintTextStyle,
+            ),
           ],
         ),
       );
